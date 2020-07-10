ffd34730cc2c4a8e4e2e9be247384ff2fce0485f
Montather Handal <ali.louy2005@gmail.com>
2020-07-10 15:06:45 -0500

    Finished

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -9,13 +9,19 @@ namespace ProblematicProblem
         static Random rng = new Random();        
         static bool cont = true;
         static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
-        private static object rnd;
 
         static void Main(string[] args)
         {
             Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
-            bool cont = bool.Parse(Console.ReadLine());
-
+            string userInput = Console.ReadLine();
+            if(userInput == "yes")
+            {
+                cont = true;
+            }
+            else
+            {
+                cont = false;
+            }
             Console.WriteLine();
 
             Console.Write("We are going to need your information first! What is your name? ");
@@ -28,9 +34,17 @@ namespace ProblematicProblem
 
             Console.WriteLine();
 
-            Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-            bool seeList = bool.Parse(Console.ReadLine());
-
+            Console.Write("Would you like to see the current list of activities? Yes/No thanks: ");
+            string ShowActivites = Console.ReadLine();
+            bool seeList;
+            if (ShowActivites == "yes")
+            {
+                seeList = true;
+            }
+            else
+            {
+                seeList = false;
+            }
             if (seeList)
             {
                 foreach (string activity in activities)
@@ -41,8 +55,19 @@ namespace ProblematicProblem
 
                 Console.WriteLine();
                 Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-                bool addToList = bool.Parse(Console.ReadLine());
+                string addToListActivities = Console.ReadLine();
                 Console.WriteLine();
+                bool addToList;
+
+                if (addToListActivities == "yes")
+                {
+                    addToList = true;
+                }
+                else
+                {
+                    addToList = false;
+                }
+                
 
                 while (addToList)
                 {
@@ -59,7 +84,15 @@ namespace ProblematicProblem
 
                     Console.WriteLine();
                     Console.WriteLine("Would you like to add more? yes/no: ");
-                    _ = bool.Parse(Console.ReadLine());
+                    string addMore = Console.ReadLine();
+                    if(addMore == "yes")
+                    {
+                        addToList = true;
+                    }
+                    else
+                    {
+                        addToList = false;
+                    }
                 }
             }
             
@@ -89,7 +122,7 @@ namespace ProblematicProblem
 
                 string randomActivity = activities[randomNumber];
 
-                if (userAge > 21 && randomActivity == "Wine Tasting")
+                if (userAge < 21 && randomActivity == "Wine Tasting")
                 {
                     Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
                     Console.WriteLine("Pick something else!");
@@ -97,12 +130,20 @@ namespace ProblematicProblem
                     activities.Remove(randomActivity);
 
                     randomNumber = rng.Next(activities.Count);
-
+                    randomActivity = activities[randomNumber];
                 }
 
-                Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
                 Console.WriteLine();
-                bool v = bool.Parse(Console.ReadLine());
+                string anotherActivity = Console.ReadLine();
+                if(anotherActivity == "keep")
+                {
+                    cont = false;
+                }
+                else
+                {
+                    cont = true;
+                }
             }
         }
     }
